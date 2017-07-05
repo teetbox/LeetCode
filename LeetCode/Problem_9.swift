04cb15ded6657f36099dac1091fd6a2362d491dc
ttionn <matt.tian@icloud.com>
2017-07-05 21:24:28 +0800

    Palindrome Number

---
+++ LeetCode/Problem_9.swift
@@ -0,0 +1,22 @@
+//
+//  Problem_9.swift
+//  LeetCode
+//
+//  Created by Matt Tian on 7/5/17.
+//  Copyright © 2017 Bizersoft. All rights reserved.
+//
+
+import Foundation
+
+// Palindrome Number
+
+/*
+ Determine whether an integer is a palindrome. Do this without extra space.
+*/
+
+class Solution_9 {
+    func isPalindrome(_ x: Int) -> Bool {
+        guard x >= 0 else { return false }
+        return String(x) == String(String(x).characters.reversed())
+    }
+}
